fc3ed32ef9cbb864fb8f8522a81b8d0565f6ecec
lntl <luc.natale@gmail.com>
2025-06-11 16:12:58 +0200

    stable

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -5,7 +5,7 @@ from calculator.calculator import add, divide, multiply, subtract
 
 class TestCalculator(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(add(1, 2), 3)
+        self.assertEqual(add(1, 5), 3)
         self.assertEqual(add(-1, 1), 0)
         self.assertEqual(add(-1, -1), -2)
 
